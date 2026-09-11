87c1cba2765ca4954a62ef245f94174e316fe159
Ahsan <ahsan@gmail.com>
2026-09-11 20:41:27 +0500

    facebook id updated again

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -69,7 +69,7 @@ export class RegisterComponent implements OnInit{
     fbScriptTag.onload = () => {
       window.fbAsyncInit = function () {
         FB.init({
-          appId: '1715739222846818',
+          appId: '2919394641754981',
           cookie: true,
           xfbml: true,
           version: 'v19.0'
